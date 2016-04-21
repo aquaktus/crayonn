9c9e38f21f8a72f482f782e90d3f4565474865f9
carlos gemmell <aquaktus@yahoo.co.uk>
2016-04-21 21:51:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express           = require('express'),
     meetupsController = require('./server/controllers/meetups-controller');
 
 mongoose.connect('mongodb://localhost:27017/mean-demo');
-//this is a hello
+//this is a bye
 
 app.use(bodyParser());
 
